fffd0f1b0c627923160a219cd3b2f5526bf44918
khushi-tripathi <KTripathi@bpl.netlink.com>
2024-03-24 13:45:11 -0700

    Summary : Profile preview setup with dummy image

--- chat-space/src/Components/Profile.jsx
+++ chat-space/src/Components/Profile.jsx
@@ -1,22 +1,120 @@
 import React from "react";
 import "../styles/profile.scss";
 import { Button , Col, Row } from "antd";
+import {
+  DownloadOutlined,
+  RotateLeftOutlined,
+  RotateRightOutlined,
+  SwapOutlined,
+  ZoomInOutlined,
+  ZoomOutOutlined,
+} from '@ant-design/icons';
+import { Image, Space } from 'antd';
+const src = 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png';
 
 export default function Profile({ user }) {
+  const onDownload = () => {
+    fetch(src)
+      .then((response) => response.blob())
+      .then((blob) => {
+        const url = URL.createObjectURL(new Blob([blob]));
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = 'image.png';
+        document.body.appendChild(link);
+        link.click();
+        URL.revokeObjectURL(url);
+        link.remove();
+      });
+  }
+
   return (
     <Row className="profile">
     <Col span={5}>
+   
     <Button
         className="photo"
         onClick={(event) => {
-          alert("HELLO");
+          // alert("HELLO");
           //agr tab uss me switch nhi krvaani h to
           event.stopPropagation();
         }}
-      />
+      >
+ <Image
+      // width={200}
+      src={src}
+      preview={{
+        // visible : true ,
+        toolbarRender: (
+          _,
+          {
+            transform: { scale },
+            actions: { onFlipY, onFlipX, onRotateLeft, onRotateRight, onZoomOut, onZoomIn },
+          },
+        ) => (
+          <Space size={12} className="toolbar-wrapper">
+            <DownloadOutlined onClick={onDownload} />
+            <SwapOutlined rotate={90} onClick={onFlipY} />
+            <SwapOutlined onClick={onFlipX} />
+            <RotateLeftOutlined onClick={onRotateLeft} />
+            <RotateRightOutlined onClick={onRotateRight} />
+            <ZoomOutOutlined disabled={scale === 1} onClick={onZoomOut} />
+            <ZoomInOutlined disabled={scale === 50} onClick={onZoomIn} />
+          </Space>
+        ),
+      }}
+    />
+
+      </Button>
     </Col>
     <Col span={19}><div className="name">{user?.first_name + " " + user?.last_name}</div></Col>
   </Row>
     
   );
 }
+
+
+// const App = () => {
+//   // or you can download flipped and rotated image
+//   // https://codesandbox.io/s/zi-ding-yi-gong-ju-lan-antd-5-7-0-forked-c9jvmp
+//   const onDownload = () => {
+//     fetch(src)
+//       .then((response) => response.blob())
+//       .then((blob) => {
+//         const url = URL.createObjectURL(new Blob([blob]));
+//         const link = document.createElement('a');
+//         link.href = url;
+//         link.download = 'image.png';
+//         document.body.appendChild(link);
+//         link.click();
+//         URL.revokeObjectURL(url);
+//         link.remove();
+//       });
+//   };
+//   return (
+    // <Image
+    //   width={200}
+    //   src={src}
+    //   preview={{
+    //     toolbarRender: (
+    //       _,
+    //       {
+    //         transform: { scale },
+    //         actions: { onFlipY, onFlipX, onRotateLeft, onRotateRight, onZoomOut, onZoomIn },
+    //       },
+    //     ) => (
+    //       <Space size={12} className="toolbar-wrapper">
+    //         <DownloadOutlined onClick={onDownload} />
+    //         <SwapOutlined rotate={90} onClick={onFlipY} />
+    //         <SwapOutlined onClick={onFlipX} />
+    //         <RotateLeftOutlined onClick={onRotateLeft} />
+    //         <RotateRightOutlined onClick={onRotateRight} />
+    //         <ZoomOutOutlined disabled={scale === 1} onClick={onZoomOut} />
+    //         <ZoomInOutlined disabled={scale === 50} onClick={onZoomIn} />
+    //       </Space>
+    //     ),
+    //   }}
+    // />
+//   );
+// };
+// export default App;
